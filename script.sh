80422d96c1e38eed371909c27d96dc7d49357612
Helio Schultz <srlhyo@gmail.com>
2025-11-06 19:12:48 +0000

    Revert "infra changes"
    This reverts commit 86c5db2cb454963ecdb205cedf9547412bff7153.

--- script.sh
+++
@@ -1,4 +0,0 @@
-docker-compose -f docker-compose.yml build
-docker-compose -f docker-compose.yml up -d
-docker cp dump.sql mysql_laravel:/dump.sql
-docker exec -i mysql_laravel sh -c 'exec mysql -ularavel -plaravel laravel' < dump.sql
